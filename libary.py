aba2c6191c705546a50c4fbac22f356021cd4923
Avraham <azoreff@gmail.com>
2025-11-10 14:57:58 +0200

    changed borrow and returm matheods

--- libary.py
+++ libary.py
@@ -2,18 +2,23 @@ class Libarty:
     def __init__(self):
         pass
     def borrow_book(user_id, book_isbn):
-        if user_id in list_of_users and book_isbn in  list_of_books  and borrow_book = False:
-            borrow_book = True
-            list_of_books.remove(book_isbn) 
-            is_available = False
-
+        for i in range(len(managment['list_of_users'])):
+            if user_id in managment['list_of_users'][i]['ID']:
+                for i in range(len(managment['list_of_books'])):
+                    if book_isbn in  managment['list_of_books'][i]['ISBN']  and borrow_book == False:
+                        borrow_book = True
+                        managment['list_of_books'][i]['is_available'] = False
+            else:
+                return "no such book"
     def return_book(user_id, book_isbn):
-        if user_id in list_of_users and  borrow_book = True:
-            list_of_books.append(book_isbn)
-            borrow_book = True
-            is_available = True
-            
-            
+         for i in range(len(managment['list_of_users'])):
+            if user_id in managment['list_of_users'][i]['ID']:
+                for i in range(len(managment['list_of_books'])):
+                    if book_isbn in  managment['list_of_books'][i]['ISBN']  and borrow_book == True:
+                        borrow_book = False
+                        managment['list_of_books'][i]['is_available'] = True
+            else:
+                return "check again your ID or book ISBN"
 
 
 
