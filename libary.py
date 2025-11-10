7a68522604a21b7b4e09aea3d52d218a1549d3ae
Avraham <azoreff@gmail.com>
2025-11-10 11:01:57 +0200

    added the second part of libarty

---
+++ libary.py
@@ -0,0 +1,21 @@
+class Libarty:
+    def __init__(self):
+        pass
+    def borrow_book(user_id, book_isbn):
+        if user_id in list_of_users and book_isbn in  list_of_books  and borrow_book = False:
+            borrow_book = True
+            list_of_books.remove(book_isbn) 
+            is_available = False
+
+    def return_book(user_id, book_isbn):
+        if user_id in list_of_users and  borrow_book = True:
+            list_of_books.append(book_isbn)
+            borrow_book = True
+            is_available = False
+
+            
+            
+
+
+
+            
