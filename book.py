388dc5c61c1b191ddb44be371279a7a094991a1e
benny_neiman <bn161299@gmail.com>
2025-11-10 09:21:16 +0200

    Canceling a commit

--- book.py
+++ book.py
@@ -1,8 +1,8 @@
 class Book:
-    def __init__(self,title,author,ISBN):
+    def __init__(self,title,author,ISBN,is_available:bool):
         self.title=title
         self.author=author
         self.ISBN=ISBN
-        self.is_available=True
+        self.is_available=is_available
     def __str__(self):
         return f"title : {self.title} , author : {self.author} , ISBN : {self.ISBN} , is available : {self.is_available}"
