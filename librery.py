38a5e13f46d18fc3c802c0ccce97b62562087c66
benny_neiman <bn161299@gmail.com>
2025-11-10 12:05:23 +0200

    Change the class to know how to add the Ad User and Ad Book functions to a Json file.

--- librery.py
+++ librery.py
@@ -1,24 +1,29 @@
 class Library:
     def __init__(self,list_of_users:list,list_of_books:list):
-        self.list_of_users=list_of_users
-        self.list_of_books=list_of_books
+        self.list_of_users=mangment["list_of_users"]
+        self.list_of_books=mangment["list_of_books"]
         
     def add_book(self,new_book):
         self.list_of_books.append(new_book)
-        return self.list_of_books
+        with open ("mangment.json","w") as f:
+            f.write (self.list_of_books)
+        
+        
     
     def add_user(self,user):
         
         self.list_of_users.append(user)
-        return self.list_of_users
+        with open ("mangment.json","w") as f:
+            f.write (self.list_of_users)
 
     def list_available_books(self):
         return self.list_of_books
     
     def search_book(self,title):
         title=input("enter a book title ")
-        if title in library_mangment["title"]:
-            return "is avalible"
-        else:
-            return "not avalible"
+        for i in range(len(mangment["list_of_books"])):
+            if title in mangment["list_of_books"][i]['title']:
+                return "is avalible"
+            else:
+                return "not avalible"
             
